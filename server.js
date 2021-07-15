f2244e8831ac8a851849a5761933627b4cfc967e
Bryan Lilly <bryanjameslilly@gmail.com>
2021-07-15 16:41:29 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+//load env vars
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+//finds the port from dotenv OR uses port 5000
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`));
