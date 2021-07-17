b7aefe2db0a7b8b5586ba1abb6e2eb9b111735f4
Bryan Lilly <bryanjameslilly@gmail.com>
2021-07-17 20:35:59 +0700

    MongoDB - CRUD

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,44 +1,110 @@
+const Bootcamp = require('../models/Bootcamp');
+
+
 //@desc     Get all bootcamps
 //@route    GET /api/v1/bootcamps
 //@access   Public
-exports.getBootcamps = (req, res, next) => {
-    res
-        .status(200)
-        .json({ success: true, msg: 'Get all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+    
+    try {
+        const bootcamps = await Bootcamp.find();
+
+        res
+            .status(200)
+            .json({ success: true, count: bootcamps.length, data: bootcamps });
+    } catch (error) {
+        res
+            .status(400)
+            .json({ success: false, msg: error.message });
+    }    
 }
 
 //@desc     Get a bootcamp with id
 //@route    GET /api/v1/bootcamps/:id
 //@access   Public
-exports.getBootcamp = (req, res, next) => {
-    res 
-        .status(200)
-        .json({ success: true, msg: `Get bootcamp with id ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+    
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        if(!bootcamp){
+            return res.status(400).json({ success: false, msg: 'Item not found' });
+        }
+
+        res
+            .status(200)
+            .json({
+                success: true,
+                data: bootcamp
+            });
+    } catch (error) {
+        res
+            .status(400)
+            .json({
+                success: false,
+                msg: error.message
+            });
+    }
 }
 
 //@desc     Create a bootcamp
 //@route    POST /api/v1/bootcamps
 //@access   Private - admin/publisher
-exports.createBootcamp = (req, res, next) => {
-    res 
-        .status(201)
-        .json({ success: true, msg: 'create new bootcamp' });
+exports.createBootcamp = async (req, res, next) => {
+        
+    try {
+        const bootcamp = await Bootcamp.create(req.body);   
+    
+        res 
+            .status(201)
+            .json({ success: true, data: bootcamp });
+    } catch (error) {
+        res.status(400).json({ success: false, msg: error.message });
+    }
+
 }
 
 //@desc     Update a bootcamp with id
 //@route    PUT /api/v1/bootcamps/:id
 //@access   Private - admin/publisher of bootcamp
-exports.updateBootcamp = (req, res, next) => {
-    res
-        .status(200)
-        .json({ success: true, msg: `Update bootcamp with id ${req.params.id}` });    
+exports.updateBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        if(!bootcamp){
+            return res.status(400).json({ success: false, msg: 'Item not found' });
+        }
+
+        res
+            .status(200)
+            .json({ success: true, data: bootcamp });
+
+    } catch (error) {
+        res.status(400).json({ success: false, msg: error.message });
+    }    
 }
 
 //@desc     Delete a bootcamp with id
 //@route    DELETE /api/v1/bootcamps/:id
 //@access   Private - admin/publisher of bootcamp
-exports.deleteBootcamp = (req, res, next) => {
-    res
-        .status(200)
-        .json({ success: true, msg: `Delete the bootcamp with id ${req.params.id}`  });  
+exports.deleteBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+        if(!bootcamp) {
+            return res.status(400).json({ success: false, msg: 'Item not found' });
+        }
+
+        res
+            .status(200)
+            .json({
+                success: true,
+                data: 'Item successfully deleted'
+            });
+    } catch (error) {
+        res.status(400).json({ success: false, msg: error.message });
+    }
 }
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan'); //bring in middleware
+const colors = require('colors');
+const connectDB = require('./config/db');
+
+//load env vars
+dotenv.config({ path: './config/config.env' });
 
 //bring route files
 const bootcamps = require('./routes/bootcamps');
 
-//bring in middleware
-const morgan = require('morgan');
+//connect to DB
+connectDB();
 
-//load env varssa
-dotenv.config({ path: './config/config.env' });
+
 
 const app = express();
+
+//body parser
+app.use(express.json());
 
 //middleware dev logger
 if(process.env.NODE_ENV === 'development') {
@@ -23,4 +31,11 @@ app.use('/api/v1/bootcamps', bootcamps)
 //finds the port from dotenv OR uses port 5000
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`.blue.bold));
+
+//unhandled promise rejections handler
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`error: ${err.message}`.red.bold);
+    //close the server
+    server.close(() => process.exit(1));
+});
